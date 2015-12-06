0e655a2d9781bd9a81d91628a99096b3b20c6b18
codeocelot <joeygracey@gmail.com>
2015-12-05 19:27:48 -0800

    implemented promises put all code in a single main.js file cleaned up console.log statements

--- main.js
+++ main.js
@@ -1,62 +1,63 @@
 'use strict'
-
+var _ = require('underscore')
 var request = require('request');
 var reqOptions = {};
-var secrets = [];
 
 function getSessionId(cb){
 	request('http://challenge.shopcurbside.com/get-session',function(err,resp,body){
-		console.log('got: ',body);
 		reqOptions.headers={'Session':body};
-		// getNode('start');
 		cb(body);
 	})
 }
 
 function getNode(id,cb,history){
 	reqOptions.url='http://challenge.shopcurbside.com/'+id;
-	request(reqOptions,(err,response,body)=>{
-		if(!body) return;
-		body = JSON.parse(body);
-		if(body.error){
-			getSessionId(()=>{
-				getNode(id,null,history);
-			})
-
-			return
-		}
-		// console.log(body)
-		if(body.secret){
-			body.history = history;
-			secrets.push(body);
-			console.log(body.secret);
-		}
-		else{
-			// var body = JSON.parse(body)
-			if(!body.next) return;
-			// console.log('body.next is', body.next);
-			if(Array.isArray(body.next)){
-				body.next.forEach((el,i)=>
-					{
-						console.log('history i', i, history)
-
-						getNode(el,null,history+i)
-					}
-				);
-			} else {
-				getNode(body.next,null,history+0);
+	return new Promise((resolve,reject)=>{
+		request(reqOptions,(err,response,body)=>{
+			if(!body) return;
+			body = JSON.parse(body);
+			if(body.error){
+				if(body.error === 'Page not found') return resolve();
+				return getSessionId(()=>{
+					resolve(getNode(id,null,history));
+				})
 			}
-		}
-		if(cb){
-			cb();
-		}
+			if(body.secret){
+				body.history = history;
+				resolve(body);
+			}
+			else{
+				if(Array.isArray(body.next)){
+					var promises = body.next.map((n,i)=>{
+						return getNode(n,null,history+i);
+					})
+					Promise.all(promises).then(results=>resolve(results))
+				} else {
+					resolve(
+						getNode(body.next,null,history+0)
+					);
+				}
+			}
+		})
+	})
+}
+
+function rmNulls(arr){
+	return arr.filter(el=>{return el !== undefined })
+}
+
+function sortNodes(arr){
+	return arr.sort((a,b)=>{
+		a = a.history;
+		b = b.history;
+		return parseInt(a,10) - parseInt(b,10); // assumption: no more than 10 branching factor
 	})
 }
 
 // kickoff
-getNode('start',null,'');
-// getSessionId((id)=>getNode(id,()=>console.log(secrets)))
-// getSessionId((id)=>{
-// 	getNode('d9f9a7c4ac0a48959f0de596c91ee90b');
-// })
-setTimeout(()=>{console.log(secrets)},5000)
+getNode('start',null,'').then(nodes=>{
+	nodes = _.flatten(nodes);
+	nodes = rmNulls(nodes);
+	var sorted = sortNodes(nodes);
+	sorted.forEach(s=>console.log(s.secret))
+});
